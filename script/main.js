8fe3915ce0f1666be4f7b8f390caea310ae29d7e
Nils <area51.nils@googlemail.com>
2015-04-04 21:39:31 +0200

    Added some more methods to show results.

--- script/main.js
+++ script/main.js
@@ -20,30 +20,53 @@ function fillList(parent, listElements) {
 	listObject.css("width", parent.outerWidth());
 	listObject.css("top", parent.offset().top + parent.outerHeight() + 5);
 	listObject.css("left", parent.offset().left);
-	listObject.show();
+
+  // Add ClickHandler
+  listObject.children("ul > li").click(function() {
+    parent.css("text", $(this).html());
+    parent.enter(); // TODO
+    listObject.hide();
+  });
+  
+  listObject.show();
 }
+
+function toType(id) {
+  return "artists";
+}
+
+
 
 $(document).ready(function() {
 	/*$.get("../php/main.php", function(data) {
-		songs = $.parseJSON(data);
-		var parent = $("#results_table");
-		parent.html("");
-		songs.forEach(function(song) {
-			parent.append(transformSongIntoDiv(song));
-		});
 	});*/
 	
 	
 	$("input").focusin(function() {
 		input = $(this);
-		$.get("../php/main.php", function(data) {
-			artists = $.parseJSON(data);
-			fillList(input, artists);
+    type = toType(input.id);
+		$.get("../php/main.php", {action: "list", type: type, startswith: input.css("text")}, function(data) {
+			items = $.parseJSON(data);
+			fillList(input, items);
 		});
 	});
 	
 	$("input").focusout(function() {
 		$("#list").hide();
 	});
+
+  // TODO
+  $("input").enter(function() {
+		input = $(this);
+    type = toType(input.id);
+		$.get("../php/main.php", {action: "list", type: "songs", where: input.css("text"), where_column: type }, function(data) {
+		  songs = $.parseJSON(data);
+		  var parent = $("#results_table");
+		  parent.html("");
+		  songs.forEach(function(song) {
+			  parent.append(transformSongIntoDiv(song));
+		  });
+		});
+  });
 	
 });
