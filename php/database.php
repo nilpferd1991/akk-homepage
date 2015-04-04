977bb4e2a6f06aac248d103fe79356c1a5f10692
Nils <area51.nils@googlemail.com>
2015-04-04 14:29:24 +0200

    Rebased code to use newer PDO methods instead of depricated mysql

---
+++ php/database.php
@@ -0,0 +1,42 @@
+<?php
+include './environ.php';
+
+$connected = False;
+$connection = NULL;
+
+function connect_to_db() {
+	global $connected, $connection;
+	global $db, $db_user, $db_host, $db_password;
+	try {
+		$connection = new PDO("mysql:dbname=$db;host=$db_host", "$db_user", "$db_password");
+	} catch (PDOException $e) {
+		echo "Connection failed: " . $e->getMessage();
+	}  
+	$connected = True;
+}
+
+function query_db($query, $variables) {
+	global $connected, $connection;
+	
+	if(!$connected) {
+		connect_to_db();
+	}
+	
+	try {
+		$statement = $connection->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+		$statement->execute($variables);
+		return $statement;
+	} catch (PDOException $e) {
+		echo "Error in query: " . $e->getMessage();
+	}
+}
+
+function query_array($query, $variables=NULL) {
+	$statement = query_db($query, $variables);
+	return $statement->fetchAll(PDO::FETCH_COLUMN);
+}
+
+function query_class($query, $variables=NULL) {
+	$statement = query_db($query, $variables);
+	return $statement->fetchAll(PDO::FETCH_CLASS);
+}
---
+++ php/environ.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+ini_set("display_errors", "On");
+
+include "./db_user.php";
+
+$db = "akk";
+$dances_db = "dances";
+$users_db = "users";
+$songs_db = "songs";
+$songlists_db = "songlists";
+$artists_db = "artists";
+$playlists_db = "playlists";
+$notes_db = "notes";
