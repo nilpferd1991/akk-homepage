6a1053b2fc1ca6413b2d2d9b1d8bb36a4a837096
Nils <area51.nils@googlemail.com>
2015-04-11 17:48:00 +0200

    Started adding a edit window

--- script/main.js
+++ script/main.js
@@ -1,18 +1,28 @@
+function capitalizeFirstLetter (string) {
+	return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
 $.widget("custom.completion", {
 	_create: function() {
 		ownElement = this;
-		this.element.autocomplete({
+		ownElement.element.html("<input></input>")
+		inputElement = ownElement.element.children("input");
+		
+		inputElement.attr("type", "text");
+		inputElement.attr("placeholder", capitalizeFirstLetter(transformDivIDIntoColumnName(inputElement)) + "...");
+		inputElement.addClass("ui-completion");
+		inputElement.autocomplete({
 			source: function(request, responseFunction) {
 				column = transformDivIDIntoColumnName($(this)[0].element);
 				$.getJSON("../php/main.php", {action: "list", type: column, term: request.term},  responseFunction);
 			},
 			minLength: 0,
 			select: function(event, ui) {
-				ownElement._trigger("result", this, {value: ui.item.value});
+				ownElement._trigger("result", none, {value: ui.item.value});
 			}
 		}).keypress(function(event) {
 			if(event.which == 13) {
-				ownElement._trigger("result", this, {value: $(this).val()});
+				ownElement._trigger("result", ownElement, {value: $(this).val()});
 			}
 		});
 	}
@@ -20,7 +30,7 @@ $.widget("custom.completion", {
 
 
 function transformSongIntoDiv(song) {
-	html = "<div class='result_row'>";
+	html = "<div class='ui-result_row'>";
 	html += "<div>" + song.title + "</div>";
 	html += "<div>" + song.artist_name + "</div>";
 	html += "<div>" + song.dance_name + "</div>";
@@ -29,7 +39,7 @@ function transformSongIntoDiv(song) {
 }
 
 function transformDivIDIntoColumnName(element) {
-	id = $(element).parent().attr("id");
+	id = $(element).parent().attr("class");
 	if (id == "title_search") {
 		return "songs";
 	} else if (id == "artist_search") {
@@ -42,6 +52,8 @@ function transformDivIDIntoColumnName(element) {
 function showResults(element, text) {
 	$(".ui-autocomplete").hide();
 	
+	console.log("kh");
+	
 	column = transformDivIDIntoColumnName(element);
 	$.getJSON("../php/main.php", {action: "search", type: column, term: text}, function(data) {
 		$("#results_table").html("");
@@ -52,16 +64,14 @@ function showResults(element, text) {
 }
 
 $(document).ready(function() {
-	$("input").val("").completion({
+	$("#searchboxes > [class$=search]").completion({
 		result : showResults
 	});
 	
-	$("#clear_box").click(function() {
-		$("input").val("");
+	$("#clear_button").click(function() {
+		$("#searchboxes input").val("");
 		$("#results_table").html("");
 	});
 	
-	$("#add_item").click(function(){
-		
-	});
+	$("#edit_window").find("[class$=search]").completion();
 });
