613448f6d67e024c0918ed3be4496365b5dd6164
Nils <area51.nils@googlemail.com>
2016-06-12 21:39:14 +0200

    Added some needed methods for the old interface.

--- akk/flask/views.py
+++ akk/flask/views.py
@@ -1,33 +1,88 @@
+import sqlalchemy
 from akk import app
 
 from akk.db.entities import Song, Artist, Dance
+from akk.db.engine import session_scope
 from akk.flask.functionality import get_results_from_stub
 from akk.utilities.json import json_answer_with_scope
 
+import flask
 
-@app.route("/db/list/songs/<stub>")
-@json_answer_with_scope
+
+@json_answer_with_scope("song_title")
 def list_songs(session, stub):
     return get_results_from_stub(session, stub, Song)
 
 
-@app.route("/db/list/artists/<stub>")
-@json_answer_with_scope
+@json_answer_with_scope("name")
 def list_artists(session, stub):
     return get_results_from_stub(session, stub, Artist)
 
 
-@app.route("/db/list/dances/<stub>")
-@json_answer_with_scope
+@json_answer_with_scope("name")
 def list_dances(session, stub):
     return get_results_from_stub(session, stub, Dance)
 
 
-@app.route("/db/search/<stub>")
-@json_answer_with_scope
+@json_answer_with_scope("song_title")
 def search_everywhere(session, stub):
-    song_results = get_results_from_stub(session, stub, Song, Song.song_title)
-    artists_results = get_results_from_stub(session, stub, Song, Artist.name)
-    dances_results = get_results_from_stub(session, stub, Song, Dance.name)
+    song_results = {value["song_id"]: value for value in get_results_from_stub(session, stub, Song, Song.song_title)}
+    song_results.update({value["song_id"]: value for value in get_results_from_stub(session, stub, Song, Artist.name)})
+    song_results.update({value["song_id"]: value for value in get_results_from_stub(session, stub, Song, Dance.name)})
 
-    return song_results + artists_results + dances_results
+    return list(song_results.values())
+
+@app.route("/db/delete_song")
+def delete_song():
+    id = flask.request.args["songID"]
+
+    with session_scope() as session:
+        try:
+            songs_to_delete = session.query(Song).filter(Song.song_id == id).one()
+            session.delete(songs_to_delete)
+            return "OK"
+        except sqlalchemy.orm.exc.NoResultFound:
+            return "Database entry not found.", 500
+
+@app.route("/db/delete/artist/<id>")
+def delete_artist(id):
+    with session_scope() as session:
+        try:
+            artist_to_delete = session.query(Artist).filter(Artist.artist_id == id).one()
+            session.delete(artist_to_delete)
+            return "OK"
+        except sqlalchemy.orm.exc.NoResultFound:
+            return "Database entry not found.", 500
+
+@app.route("/db/delete/dance/<id>")
+def delete_dance(id):
+    with session_scope() as session:
+        try:
+            dances_to_delete = session.query(Dance).filter(Dance.dance_id == id).one()
+            session.delete(dances_to_delete)
+            return "OK"
+        except sqlalchemy.orm.exc.NoResultFound:
+            return "Database entry not found.", 500
+
+
+@app.route("/db/completion")
+def completion():
+    search_column = flask.request.args["type"]
+    search_stub = flask.request.args["term"]
+
+    if search_column == "search":
+        return search_everywhere(search_stub)
+    elif search_column == "songs":
+        return list_songs(search_stub)
+    elif search_column == "artists":
+        return list_artists(search_stub)
+    elif search_column == "dances":
+        return list_dances(search_stub)
+
+@app.route("/db/get_song")
+@json_answer_with_scope("song_title")
+def get_song(session):
+
+    song_id = flask.request.args["songID"]
+
+    return [session.query(Song).filter(Song.song_id == song_id).one().get_dict()]
--- akk/utilities/json.py
+++ akk/utilities/json.py
@@ -2,17 +2,27 @@ import flask
 
 class JSONBase:
     def get_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        result_dict = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+        for attr in vars(self.__class__):
+            attr_value = getattr(self, attr)
+            if isinstance(attr_value, JSONBase):
+                result_dict.update({attr + "_" + c.name: getattr(attr_value, c.name) for c in attr_value.__table__.columns})
+
+        return result_dict
 
 
-def json_answer_with_scope(func):
-    def func_wrapper(*args, **kwargs):
-        from akk.db.engine import session_scope
-        with session_scope() as session:
-            results = func(session, *args, **kwargs)
+def json_answer_with_scope(named_item):
+    def decorator(func):
+        def func_wrapper(*args, **kwargs):
+            from akk.db.engine import session_scope
+            with session_scope() as session:
+                results = func(session, *args, **kwargs)
 
-            return flask.jsonify({"results": results})
+                return flask.jsonify({"results": results, "names": [song[named_item] for song in results if named_item]})
 
-    # Nasty stuff for app.route!
-    func_wrapper.__name__ = func.__name__
-    return func_wrapper
+        # Nasty stuff for app.route!
+        func_wrapper.__name__ = func.__name__
+        return func_wrapper
+
+    return decorator
