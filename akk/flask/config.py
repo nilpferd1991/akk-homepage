1ba70e9dbb0d1e4c68337fad8d1a215180746394
Nils <area51.nils@googlemail.com>
2016-06-12 21:38:18 +0200

    Add some test data to the db.

--- akk/flask/config.py
+++ akk/flask/config.py
@@ -1,4 +1,6 @@
 import pkg_resources
+from akk.db.engine import session_scope
+from akk.db.entities import *
 from flask import g
 
 from akk import app
@@ -12,3 +14,23 @@ app.config.from_envvar('AKK_SETTINGS', silent=True)
 def initdb_command():
     from akk.db.engine import init_db
     init_db()
+    with session_scope() as s:
+        tango = Dance(name="Tango")
+        s.add(tango)
+
+        artist = Artist(name="Test Artist")
+        s.add(artist)
+
+        song = Song(song_title="Test Song", artist=artist, dance=tango)
+        s.add(song)
+
+        another_song = Song(song_title="Another Test Song", artist=artist, dance=tango)
+        s.add(song)
+
+        user = User(name="Herbert", password_hash="1", password_salt="2")
+        s.add(user)
+
+        playlist = Playlist(user=user, songs=[song, another_song])
+        s.add(playlist)
+
+        s.commit()
